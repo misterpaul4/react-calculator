94689203ad52a2dea42779242ff9f837b931411e
paul <ajuzepaul@gmail.com>
2021-02-05 21:59:49 +0200

    Remove commented code

--- src/component/Quote.js
+++ src/component/Quote.js
@@ -1,13 +1,9 @@
-/* eslint-disable no-console */
-/* eslint-disable no-unused-vars */
-/* eslint-disable max-len */
 import React, { useState } from 'react';
-// import dd from '../utils/quoteGenerator';
 
 const Quote = () => {
   const [quoteObj, generateQuote] = useState({
-    text: '',
-    author: '',
+    text: 'The price of greatness is responsibility.',
+    author: 'Winston Churchill',
   });
 
   const random = max => Math.floor(Math.random() * Math.floor(max));
@@ -16,21 +12,21 @@ const Quote = () => {
     fetch('https://type.fit/api/quotes')
       .then(response => response.json())
       .then(data => {
-        const quote = data[random(data.length)];
-        console.log(quote);
+        const rand = random(data.length);
+        const quote = data[rand];
         generateQuote({
-          text: quote.text,
           author: quote.author,
+          text: quote.text,
         });
       });
   };
 
   return (
-    <div className="container text-center">
+    <div>
       <button type="button" onClick={handClick}>generate</button>
       <h1>Quote</h1>
       <p>{quoteObj.text}</p>
-      <h1>author</h1>
+      <h1>{quoteObj.author ? 'Author' : ''}</h1>
       <p>{quoteObj.author}</p>
     </div>
   );
